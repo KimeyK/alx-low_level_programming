2be8efb01087f55bfbd72b2f6dd46dfceec92b93
KimeyK <simonkaranja8@gmail.com>
2022-03-17 16:48:42 +0300

    my commit message

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#inlude "main.h"[D[D[D[D[D[D[D[D[D[D[D[D[Dc[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[Dlude        [D[D[D[D[D[D[D[D[D[D[D[C[C[C[C"main.h"
+/**
+ *main - main
+ *Return: 0
+ */
+
+int main(void)
+{
+int i;
+i = 0;
+
+positive_or_negative(i);
+
+return (0);
+
+}
