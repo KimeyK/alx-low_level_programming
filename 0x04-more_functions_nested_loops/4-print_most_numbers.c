536be78ec520104aa3c7bf5468a1c1258de4a04a
KimeyK <simonkaranja8@gmail.com>
2022-03-18 23:06:28 +0300

    My message commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+
+int i;
+
+i = o;
+[A[C[C[C[C[0[C[B[A
+while (i < 10)
+{
+if (i != 2 && i != 4)
+{
+_putchar (i + '0');
+}
+i++;
+}
+
+_putchar ('\n');
+}
